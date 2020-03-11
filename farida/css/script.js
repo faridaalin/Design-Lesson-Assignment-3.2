a6c89217864dc1d89b9b7dbb0747cd593f490657
faridaalin <56302698+faridaalin@users.noreply.github.com>
2020-03-11 14:39:23 +0100

    page update

---
+++ farida/css/script.js
@@ -0,0 +1,20 @@
+const formContainer = document.querySelector(".container");
+const modal = document.querySelector(".modal");
+
+const submit = document.querySelector(".btn-primary");
+submit.addEventListener("click", showModal);
+
+function showModal(event) {
+  event.preventDefault();
+
+  formContainer.style.display = "none";
+  modal.classList.remove("modal");
+}
+
+const deleteButton = document.querySelector('.btn-secondary')
+deleteButton.addEventListener("click", deleteAccount);
+
+console.dir(deleteButton)
+function deleteAccount(event) {
+
+}  
